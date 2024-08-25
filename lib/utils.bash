c31fd9dc2bf309ed0d0361cf508bfc2792faa323
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2024-08-25 15:49:09 +0200

    refactor: rework list-all

---
+++ lib/utils.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+GH_REPO="https://github.com/derailed/k9s"
+TOOL_NAME="k9s"
+
+fail() {
+  printf "asdf-%s: %s\n" $TOOL_NAME "$*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+}
+
+list_all_versions() {
+  list_github_tags
+}
