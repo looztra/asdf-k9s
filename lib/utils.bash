1770ff2cfb2e3e31ccc9efb390e9d53751200154
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2024-08-25 16:03:17 +0200

    refactor: move some funcs to utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -29,3 +29,162 @@ list_github_tags() {
 list_all_versions() {
   list_github_tags
 }
+
+# Borrowed to someone, but I don't remember who it was, sorry :(
+# Print message $2 with log-level $1 to STDERR, colorized if terminal
+# log DEBUG "DOCKER_HOST ${DOCKER_HOST}"
+function log() {
+  local level=${1?}
+  shift
+  local code
+  local line
+  code=''
+  line="[$(date '+%F %T')] $level: $*"
+  if [ -t 2 ]; then
+    case "$level" in
+    INFO) code=36 ;;
+    DEBUG) code=35 ;;
+    WARN) code=33 ;;
+    ERROR) code=31 ;;
+    *) code=37 ;;
+    esac
+    echo -e "\e[${code}m${line} \e[94m(${FUNCNAME[1]})\e[0m"
+  else
+    echo "$line"
+  fi >&2
+}
+
+# from https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+function vercomp() {
+  if [[ "$1" == "$2" ]]; then
+    return 0
+  fi
+  local IFS=.
+  # shellcheck disable=SC2206
+  local i ver1=($1) ver2=($2)
+  # fill empty fields in ver1 with zeros
+  for ((i = ${#ver1[@]}; i < ${#ver2[@]}; i++)); do
+    ver1[i]=0
+  done
+  for ((i = 0; i < ${#ver1[@]}; i++)); do
+    if [[ -z ${ver2[i]} ]]; then
+      # fill empty fields in ver2 with zeros
+      ver2[i]=0
+    fi
+    if ((10#${ver1[i]} > 10#${ver2[i]})); then
+      return 1
+    fi
+    if ((10#${ver1[i]} < 10#${ver2[i]})); then
+      return 2
+    fi
+  done
+  return 0
+}
+
+get_version_short() {
+  local version=$1
+  echo "$version" | tr -d "v"
+}
+
+get_platform() {
+  echo "$(uname)_$(get_cpu)"
+}
+
+get_cpu() {
+  local machine_hardware_name
+  machine_hardware_name="$(uname -m)"
+
+  case "$machine_hardware_name" in
+  'x86_64')
+    vercomp "$version" "0.26.7"
+    if [[ $? == 1 ]]; then local cpu_type="amd64"; else local cpu_type="x86_64"; fi
+    ;;
+  'powerpc64le' | 'ppc64le') local cpu_type="ppc64le" ;;
+  'aarch64') local cpu_type="arm64" ;;
+  'armv5l' | 'armv6l' | 'armv7l') local cpu_type="arm" ;;
+  *) local cpu_type="$machine_hardware_name" ;;
+  esac
+
+  echo "$cpu_type"
+}
+
+get_filename_post_0_14_0() {
+  local version="$1"
+  local platform="$2"
+
+  echo "${TOOL_NAME}_${platform}.tar.gz"
+}
+
+get_filename_pre_0_14_0() {
+  local version="$1"
+  local platform="$2"
+
+  echo "${TOOL_NAME}_${version}_${platform}.tar.gz"
+}
+
+get_filename_0_24_10() {
+  local version="$1"
+  local platform="$2"
+
+  echo "${TOOL_NAME}_v${version}_${platform}.tar.gz"
+}
+
+k9s_get_download_url() {
+  local version="$1"
+  local platform="$2"
+  local filename
+  local path_version
+
+  # https://github.com/derailed/k9s/releases/download/v0.16.1/k9s_Linux_x86_64.tar.gz
+  # https://github.com/derailed/k9s/releases/download/0.11.1/k9s_0.11.1_Linux_x86_64.tar.gz
+  vercomp "$version" "0.14.0"
+  case $? in
+  0) op='=' ;;
+  1) op='>' ;;
+  2) op='<' ;;
+  esac
+  if [[ "$op" == '<' ]]; then
+    filename="$(get_filename_pre_0_14_0 "$version" "$platform")"
+  else
+    filename="$(get_filename_post_0_14_0 "$version" "$platform")"
+  fi
+
+  vercomp "$version" "0.13.0"
+  case $? in
+  0) op='=' ;;
+  1) op='>' ;;
+  2) op='<' ;;
+  esac
+  if [[ "$op" == '<' ]]; then
+    path_version="$version"
+  else
+    path_version="v$version"
+  fi
+
+  vercomp "$version" "0.24.10"
+  case $? in
+  0) op='=' ;;
+  1) op='>' ;;
+  2) op='<' ;;
+  esac
+  if [[ "$op" == '=' ]]; then
+    filename="$(get_filename_0_24_10 "$version" "$platform")"
+    path_version="v$version"
+  else
+    : # do not alter behavior
+  fi
+
+  vercomp "$version" "0.25.0"
+  case $? in
+  0) op='=' ;;
+  1) op='>' ;;
+  2) op='<' ;;
+  esac
+  if [[ "$op" == '<' ]] && [[ "$platform" == "Darwin_arm64" ]]; then
+    fail "Version $version is not supported on platform $platform"
+  else
+    : # do not alter behavior
+  fi
+
+  echo "https://github.com/derailed/k9s/releases/download/${path_version}/${filename}"
+}
